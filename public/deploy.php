58e8da8f257db853848c33dfbaf8c3e5876d35e8
Mark B <cw.prime@gmail.com>
2022-10-17 23:38:41 -0500

    added submodule back

--- public/deploy.php
+++ public/deploy.php
@@ -11,7 +11,10 @@
         'echo $PWD',
         'whoami',
         'git pull',
-        'git status'
+        'git status',
+        'git submodule sync',
+        'git submodule update',
+        'git submodule status',
 
     );
 
